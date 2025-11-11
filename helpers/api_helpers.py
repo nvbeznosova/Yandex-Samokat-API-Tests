b7e1a8fd6f4d6f861d620ea30bc957103a6045a5
Nadia Beznosova <beznosovanadia@yandex.ru>
2025-11-11 20:05:05 +0300

    Удалены лишние файлы из проекта. Урлы вынесены в отдельный файл с переменными

--- helpers/api_helpers.py
+++ helpers/api_helpers.py
@@ -1,29 +1,24 @@
 import requests
-
-BASE_URL = "https://qa-scooter.praktikum-services.ru/api/v1"
-
-COURIER_ENDPOINT = f"{BASE_URL}/courier"
-COURIER_LOGIN_ENDPOINT = f"{BASE_URL}/courier/login"
-ORDER_ENDPOINT = f"{BASE_URL}/orders"
+from helpers.endpoints import Endpoints
 
 
 class ApiClient:
     @staticmethod
     def create_courier(payload):
-        return requests.post(COURIER_ENDPOINT, json=payload)
+        return requests.post(Endpoints.COURIER, json=payload)
 
     @staticmethod
     def login_courier(payload):
-        return requests.post(COURIER_LOGIN_ENDPOINT, json=payload)
+        return requests.post(Endpoints.COURIER_LOGIN, json=payload)
 
     @staticmethod
     def delete_courier(courier_id):
-        return requests.delete(f"{COURIER_ENDPOINT}/{courier_id}")
+        return requests.delete(f"{Endpoints.COURIER}/{courier_id}")
 
     @staticmethod
     def create_order(payload):
-        return requests.post(ORDER_ENDPOINT, json=payload)
+        return requests.post(Endpoints.ORDERS, json=payload)
 
     @staticmethod
     def get_orders():
-        return requests.get(ORDER_ENDPOINT)
+        return requests.get(Endpoints.ORDERS)
---
+++ helpers/endpoints.py
@@ -0,0 +1,7 @@
+BASE_URL = "https://qa-scooter.praktikum-services.ru/api/v1"
+
+
+class Endpoints:
+    COURIER = f"{BASE_URL}/courier"
+    COURIER_LOGIN = f"{BASE_URL}/courier/login"
+    ORDERS = f"{BASE_URL}/orders"
